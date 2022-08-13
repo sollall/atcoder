06c8b323c1173933f432ee26973bcb54081c40b8
sollall <komugikoakasya@gmail.com>
2022-08-14 00:22:45 +0900

    primeを関数化

--- prime.py
+++ prime.py
@@ -1,13 +1,13 @@
 #素数を列挙
-N=10**5+1
+def get_prime(N):
 
-prime=[i for i in range(N)]
+    prime=[i for i in range(N)]
 
-for n in range(2,N):
-    if prime[n]==n:
-        for i in range(n,N)[::n]:
-            prime[i]=n
+    for n in range(2,N):
+        if prime[n]==n:
+            for i in range(n,N)[::n]:
+                prime[i]=n
 
-prime=sorted(list(set(prime))[2:])
+    prime=sorted(list(set(prime))[2:])
 
-print(prime)
+    return prime
